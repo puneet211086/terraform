d4ccf43ee3d1dbd27e692e8a189ea8a456bd4864
Puneet <puneet211086@gmail.com>
2024-04-28 22:22:54 -0400

    terraform code created

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+
+resource "aws_instance" "vm1" {
+
+  ami           = "ami-04e5276ebb8451442"
+  instance_type = "t2.micro"
+
+  tags = {
+    name = "my-demo-instance"
+  }
+
+}
+
+
+
+
+# resource "aws_instance" "instantance1" {
+#   ami           = "ami-04e5276ebb8451442"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     name = "my-demo-instance"
+#   }
+
+# }
